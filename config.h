b31aaedd52f21c67c124f2ceba5ea11bb57a2cee
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-01-09 20:34:39 +0200

    move to bottom

--- config.h
+++ config.h
@@ -1,15 +1,15 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"inconsolata:size=12"
+	"inconsolata:size=16"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ffffff", "#000000" },
+	[SchemeNorm] = { "#ffffff", "#222222" },
 	[SchemeSel] = { "#ffffff", "#555555" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
